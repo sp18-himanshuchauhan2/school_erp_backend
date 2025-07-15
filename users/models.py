cd530fa4a2e4af7f418c420e9c880d39e1d4e5b6
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-15 16:40:47 +0530

    created custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser, PermissionsMixin):
+    ROLE_CHOICES = [
+        ('MAIN_ADMIN', 'Main Admin'),
+        ('SCHOOL_ADMIN', 'School Admin'),
+        ('TEACHER', 'Teacher'),
+        ('STUDENT', 'Student'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    school = models.ForeignKey('schools.School', on_delete=models.SET_NULL, null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'role']
+    
+    def __str__(self):
+        return f"{self.name} --> ({self.role})"
+    
