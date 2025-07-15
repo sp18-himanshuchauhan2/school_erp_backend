67861dd15392512468b17846169a95aa18821e63
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-15 16:42:08 +0530

    registered user on django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'name', 'role', 'school', 'is_staff', 'is_superuser']
+    list_filter = ['role', 'school']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name', 'phone', 'address')}),
+        ('School Info', {'fields': ('role', 'school')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    search_fields = ['email']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'role', 'school', 'phone', 'password1', 'password2'),
+        }),
+    )
+
+# admin.site.register(User, UserAdmin)
