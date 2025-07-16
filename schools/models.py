679f75cc736e92878e19bc70319ba2c9367e225d
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-16 15:56:07 +0530

    added time-stamped-model using django extensions

--- schools/models.py
+++ schools/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 # Create your models here.
 
-class School(models.Model):
+class School(TimeStampedModel):
     name = models.CharField(max_length=100)
     address = models.TextField()
     contact_email = models.EmailField()
     contact_number = models.CharField(max_length=15)
     established_year = models.PositiveIntegerField()
-    # django extensions -> timestamp
+
     def __str__(self):
         return self.name
