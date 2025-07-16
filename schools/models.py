f06e94ba8de4f6a1d8a55af336dc047828f7e948
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-16 11:11:12 +0530

    created school model

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    contact_email = models.EmailField()
+    contact_number = models.CharField(max_length=15)
+    established_year = models.PositiveIntegerField()
+    # django extensions -> timestamp
+    def __str__(self):
+        return self.name
