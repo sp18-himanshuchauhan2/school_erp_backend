48ab32a01941435fdfc6d252a9a1b7e9bb16de8d
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-16 11:12:30 +0530

    registered school on django admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import School
 
 # Register your models here.
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'contact_email', 'contact_number', 'established_year']
+    search_fields = ['name', 'contact_email']
