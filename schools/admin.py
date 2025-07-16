094140f10db5c96796105f65333afec51911ab3c
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-16 15:57:51 +0530

    added get-queryset function and filter only school users

--- schools/admin.py
+++ schools/admin.py
@@ -6,3 +6,11 @@ from .models import School
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ['name', 'contact_email', 'contact_number', 'established_year']
     search_fields = ['name', 'contact_email']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        if hasattr(request.user, "role") and request.user.role == "SCHOOL_ADMIN":
+            return qs.filter(id=request.user.school_id)
+        return qs.none()
