c9dd0d1f9861175a034367c146f9f71a7e4cb83d
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-17 15:35:00 +0530

    created classroom model

--- classrooms/models.py
+++ classrooms/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from schools.models import School
 
 # Create your models here.
+
+class Classroom(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='classrooms')
+    class_name = models.CharField(max_length=10)
+    section = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        unique_together = ('school', 'class_name', 'section')
+
+    def __str__(self):
+        return f"{self.class_name} - {self.section or ''} -> {self.school}"
