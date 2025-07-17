372a6a7d68e644834d7d7df6fb61b589fcbf2829
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-17 15:36:08 +0530

    registered classroom on django admin

--- classrooms/admin.py
+++ classrooms/admin.py
@@ -3,3 +3,21 @@ from .models import Classroom
 from schools.models import School
 
 # Register your models here.
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['school', 'class_name', 'section']
+    search_fields = ['class_name', 'section']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(school=request.user.school)
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'school' and not request.user.is_superuser:
+            kwargs['queryset'] = School.objects.filter(id=request.user.school.id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    
+admin.site.register(Classroom, ClassroomAdmin)
