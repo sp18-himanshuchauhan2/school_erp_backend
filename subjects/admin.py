a8cf28261ffa417bd5a4fb502ec710acc8e951db
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-18 14:54:04 +0530

    created subjects app

---
+++ subjects/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Subject, ClassroomSubject
+from schools.models import School
+from classrooms.models import Classroom
+from teachers.models import Teacher
+
+# Register your models here.
---
+++ subjects/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from classrooms.models import Classroom
+from teachers.models import Teacher
+from schools.models import School
+
+# Create your models here.
