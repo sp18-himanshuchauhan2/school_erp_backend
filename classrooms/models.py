2869f40f3315efdb7996755ca17585155539bd56
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-18 11:16:55 +0530

    added Teacher model as a FK for class-teacher

--- classrooms/models.py
+++ classrooms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from schools.models import School
+from teachers.models import Teacher
 
 # Create your models here.
 
@@ -7,9 +8,10 @@ class Classroom(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='classrooms')
     class_name = models.CharField(max_length=10)
     section = models.CharField(max_length=10, blank=True, null=True)
+    class_teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True, related_name='classrooms_as_teacher')
 
     class Meta:
-        unique_together = ('school', 'class_name', 'section')
+        unique_together = ('school', 'class_name', 'section', 'class_teacher')
 
     def __str__(self):
-        return f"{self.class_name} - {self.section or ''} -> {self.school}"
+        return f"{self.class_name} - {self.section or ''} ({self.school})"
