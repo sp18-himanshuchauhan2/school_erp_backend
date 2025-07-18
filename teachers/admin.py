e06d506dfe7de7029eb94d8ac6b4f05a24df4488
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-18 14:52:06 +0530

    registered teacher on django admin

--- teachers/admin.py
+++ teachers/admin.py
@@ -4,3 +4,30 @@ from schools.models import School
 from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+User = get_user_model()
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subject_spec', 'qualification', 'experience', 'gender', 'dob', 'join_date', ]
+    search_fields = ['user__name', 'subject_spec']
+
+    def get_queryset(self, request):
+        qs =  super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        try:
+            school = School.objects.get(user=request.user)
+            return qs.filter(user__school=school)
+        except School.DoesNotExist:
+            return Teacher.objects.none()
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'user' and not request.user.is_superuser:
+            try:
+                school = School.objects.get(user=request.user)
+                kwargs['queryset'] = User.objects.filter(school=school)
+            except School.DoesNotExist:
+                kwargs['queryset'] = User.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Teacher, TeacherAdmin)
