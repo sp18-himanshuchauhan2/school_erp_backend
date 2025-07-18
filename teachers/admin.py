fcd88fb5b49d9072b8fe7c9558749c342ebda6ce
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-18 14:48:29 +0530

    created teachers app

---
+++ teachers/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Teacher
+from schools.models import School
+from django.contrib.auth import get_user_model
+
+# Register your models here.
---
+++ teachers/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from config import settings
+from schools.models import School
+
+# Create your models here.
