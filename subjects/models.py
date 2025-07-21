e179f1f1c1f1b857a1311f983ea1188b080507c3
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-21 13:16:20 +0530

    made unique together subject with school

--- subjects/models.py
+++ subjects/models.py
@@ -10,7 +10,7 @@ class Subject(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        unique_together = ('name',)
+        unique_together = ('school', 'name',)
 
     def __str__(self):
         return self.name
