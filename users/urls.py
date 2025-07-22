5b2f569341459c98bcda562e3af6a0cd0c19ba1d
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 15:42:14 +0530

    updated urls and remove duplicates

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user as user_views, school_admin as views
+from .views import user as user_views
 from classrooms import views as classroom_views
 from subjects.views import subject as subject_views, classroom_subject as classroom_subject_views
 from teachers import views as teacher_views
@@ -11,29 +11,26 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # school-admin views
-    path('users/', views.SchoolAdminUserListView.as_view(), name='school_users'),
-    path('classrooms/', views.SchoolAdminClassroomListView.as_view(),
-         name='school_classrooms'),
-    path('subjects/', views.SchoolAdminSubjectListView.as_view(),
-         name='school_subjects'),
-    path('classroom-subjects/', views.SchoolAdminClassroomSubjectListView.as_view(),
-         name='school_classroom_subjects'),
+    # path('users/', views.SchoolAdminUserListView.as_view(), name='school_users'),
+    # path('classrooms/', views.SchoolAdminClassroomListView.as_view(), name='school_classrooms'),
+    # path('subjects/', views.SchoolAdminSubjectListView.as_view(), name='school_subjects'),
+    # path('classroom-subjects/', views.SchoolAdminClassroomSubjectListView.as_view(), name='school_classroom_subjects'),
 
     # CRUD
-    # path('users/', user_views.ListUsersView.as_view(), name='list-users'),
-    path('users/create/', user_views.CreateUserView.as_view(), name='create-user'),
+    path('users/', user_views.ListCreateUsersView.as_view(),
+         name='list-create-user'),
 
-    path('classrooms/create/', classroom_views.ClassroomListCreateView.as_view(),
+    path('classrooms/', classroom_views.ClassroomListCreateView.as_view(),
          name='classroom-create'),
     path('classrooms/<int:pk>/',
          classroom_views.ClassroomDetailView.as_view(), name='classroom-detail'),
 
-    path('subjects/create/', subject_views.SubjectListCreateView.as_view(),
+    path('subjects/', subject_views.SubjectListCreateView.as_view(),
          name='subject-create'),
     path('subjects/<int:pk>/', subject_views.SubjectDetailView.as_view(),
          name='subject-detail'),
 
-    path('classroom-subjects/create/',
+    path('classroom-subjects/',
          classroom_subject_views.ClassroomSubjectListCreateView.as_view(), name='cs-create'),
     path('classroom-subjects/<int:pk>/',
          classroom_subject_views.ClasroomSubjectDetailView.as_view(), name='cs-detail'),
