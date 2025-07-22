155beff5c14c8caa19fd3c7a87e89c54956bf7f4
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 13:04:30 +0530

    added urls

---
+++ users/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .views import user as user_views, school_admin as views
+from classrooms import views as classroom_views
+from subjects.views import subject as subject_views, classroom_subject as classroom_subject_views
+from teachers import views as teacher_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    # JWT login
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # school-admin views
+    path('users/', views.SchoolAdminUserListView.as_view(), name='school_users'),
+    path('classrooms/', views.SchoolAdminClassroomListView.as_view(), name='school_classrooms'),
+    path('subjects/', views.SchoolAdminSubjectListView.as_view(), name='school_subjects'),
+    path('classroom-subjects/', views.SchoolAdminClassroomSubjectListView.as_view(), name='school_classroom_subjects'),
+
+    # CRUD 
+    # path('users/', user_views.ListUsersView.as_view(), name='list-users'),
+    path('users/create/', user_views.CreateUserView.as_view(), name='create-user'),
+
+    path('classrooms/create/', classroom_views.ClassroomListCreateView.as_view(), name='classroom-create'),
+    path('classrooms/<int:pk>/', classroom_views.ClassroomDetailView.as_view(), name='classroom-detail'),
+
+    path('subjects/create/', subject_views.SubjectListCreateView.as_view(), name='subject-create'),
+    path('subjects/<int:pk>/', subject_views.SubjectDetailView.as_view(), name='subject-detail'),
+
+    path('classroom-subjects/create/', classroom_subject_views.ClassroomSubjectListCreateView.as_view(), name='cs-create'),
+    path('classroom-subjects/<int:pk>/', classroom_subject_views.ClasroomSubjectDetailView.as_view(), name='cs-detail'),
+
+    path('teacher/', teacher_views.TeacherListCreateView.as_view(), name='teacher-list-create'),
+    path('teacher/<int:pk>/', teacher_views.TeacherRetrieveUpdateDestroyView.as_view(), name='teacher-detail'),
+]
