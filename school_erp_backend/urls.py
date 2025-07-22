33bd456be8ec99e82dbd5c3a37b299892d383cde
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 14:42:25 +0530

    added swagger urls

--- school_erp_backend/urls.py
+++ school_erp_backend/urls.py
@@ -15,9 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="School ERP Backend API",
+        default_version='v1',
+        description="API documentation for School ERP system",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="himanshu.chauhan2@sparkeighteen.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/school-admin/', include('users.urls'))
+    path('api/school-admin/', include('users.urls')),
+
+    # Swagger
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
