afb4dead622cec3a950ebafe9ee563acf3bcdf4f
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 13:02:26 +0530

    made subject views

---
+++ subjects/views/subject.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+from ..models import Subject
+from ..serializers.subject import SubjectSerializer
+
+class SubjectListCreateView(generics.ListCreateAPIView):
+    serializer_class = SubjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Subject.objects.filter(school=self.request.user.school)
+    
+class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = SubjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Subject.objects.filter(school=self.request.user.school)
+    
