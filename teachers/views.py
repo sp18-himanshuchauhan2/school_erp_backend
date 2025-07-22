8c911a5a53984136b8a39358089e783f4fe6140e
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 13:03:30 +0530

    made teacher views

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Teacher
+from .serializers import TeacherSerializer
 
-# Create your views here.
+class TeacherListCreateView(generics.ListCreateAPIView):
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Teacher.objects.filter(user__school=self.request.user.school)
+    
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+class TeacherRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Teacher.objects.filter(user__school=self.request.user.school)
+
+    def get_serializer_context(self):
+        return {'request': self.request}
