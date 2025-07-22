945c27bf9e8b2299beaa0dfb31da41bbb0406f0d
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 13:03:14 +0530

    made teacher serializers

---
+++ teachers/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from teachers.models import Teacher
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['school'] = self.context['request'].user.school
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        validated_data.pop('school', None)
+        return super().update(instance, validated_data)
