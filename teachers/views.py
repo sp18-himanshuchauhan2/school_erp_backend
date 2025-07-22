cd00948aef56a721876ce304f0dcce51530a8a96
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 17:40:57 +0530

    updated teacher views

--- teachers/views.py
+++ teachers/views.py
@@ -1,6 +1,9 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .models import Teacher
+from users.models import User
 from .serializers import TeacherSerializer
+from users.serializers import UserListSerializer
 
 
 class TeacherListCreateView(generics.ListCreateAPIView):
@@ -12,6 +15,22 @@ class TeacherListCreateView(generics.ListCreateAPIView):
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+    def list(self, request, *args, **kwargs):
+        school = request.user.school
+        teachers = self.get_queryset()
+        teachers_data = TeacherSerializer(
+            teachers, many=True, context={'request': request}).data
+
+        teachers_users_id = teachers.values_list('user_id', flat=True)
+        user_without_profile = User.objects.filter(
+            role='TEACHER', school=school).exclude(id__in=teachers_users_id)
+        user_data = UserListSerializer(user_without_profile, many=True, context={
+                                       'request': request}).data
+        return Response({
+            "teachers": teachers_data,
+            "users_with_teacher_role": user_data
+        }, status=status.HTTP_200_OK)
 
 
 class TeacherRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
