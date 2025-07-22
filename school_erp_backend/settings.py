bbceee72bc6f1e5ba6f55b5b60ace461a95d5ef1
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-22 15:38:22 +0530

    added swagger settings for JWT Auth

--- school_erp_backend/settings.py
+++ school_erp_backend/settings.py
@@ -147,3 +147,15 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
 }
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'JWT token. Format: Bearer <token>',
+        }
+    },
+}
