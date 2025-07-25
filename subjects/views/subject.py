f0b53e79cf1524493faab7b491faa880f109acfa
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-25 13:25:22 +0530

    created api for bulk upload

--- subjects/views/subject.py
+++ subjects/views/subject.py
@@ -1,6 +1,13 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from ..models import Subject
 from ..serializers.subject import SubjectSerializer
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+import openpyxl
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class SubjectListCreateView(generics.ListCreateAPIView):
@@ -17,3 +24,46 @@ class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Subject.objects.filter(school=self.request.user.school)
+
+class SubjectBulkUploadView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+            operation_description="Upload Excel file to bulk create classrooms.",
+            manual_parameters=[
+                openapi.Parameter(
+                    name='file',
+                    in_=openapi.IN_FORM,
+                    type=openapi.TYPE_FILE,
+                    required=True,
+                    description="Excel file (.xlsx) with class_name, section, class_teacher",
+                )
+            ],
+            responses={201: "Successfully created classrooms", 400: "Bad Request"},
+    )
+    def post(self, request, *args, **kwargs):
+        excel_file = request.FILES.get('file')
+        if not excel_file:
+            return Response({"error": "No file uploaded."}, status=400)
+        
+        try:
+            wb = openpyxl.load_workbook(excel_file)
+            sheet = wb.active
+        except Exception as e:
+            return Response({"error": f"Invalid Excel file: {str(e)}"}, status=400)
+        
+        created = []
+        existed = set(name.lower() for name in Subject.objects.filter(school=request.user.school).values_list('name', flat=True))
+
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            name = row[0]
+            if name and name.lower() not in existed:
+                created.append(Subject(name=name.strip(), school=request.user.school))
+                existed.add(name.lower())
+
+        if created:
+            Subject.objects.bulk_create(created)
+            return Response({"message": f"{len(created)} subjects uploaded successfully."}, status=200)
+        else:
+            return Response({"message": "No new subjects to upload. All are duplicates."}, status=200)
