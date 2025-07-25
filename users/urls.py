46e5ce6faa26258238e329d23c398b2671ed80cc
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-25 13:25:54 +0530

    updated urls

--- users/urls.py
+++ users/urls.py
@@ -7,40 +7,43 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from students.views import StudentListCreateAPIView, StudentRetrieveUpdateDeleteAPIView
 
 urlpatterns = [
-    # JWT login
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # JWT Auth
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # school-admin views
-    # path('users/', views.SchoolAdminUserListView.as_view(), name='school_users'),
-    # path('classrooms/', views.SchoolAdminClassroomListView.as_view(), name='school_classrooms'),
-    # path('subjects/', views.SchoolAdminSubjectListView.as_view(), name='school_subjects'),
-    # path('classroom-subjects/', views.SchoolAdminClassroomSubjectListView.as_view(), name='school_classroom_subjects'),
-
-    # CRUD
+    # Users
     path('users/', user_views.ListCreateUsersView.as_view(),
          name='list-create-user'),
 
+    # Classrooms
     path('classrooms/', classroom_views.ClassroomListCreateView.as_view(),
          name='classroom-create'),
+    path('classrooms/bulk/', classroom_views.ClassroomBulkUploadAPIView.as_view(),
+         name='classroom-bulk-upload'),
     path('classrooms/<int:pk>/',
          classroom_views.ClassroomDetailView.as_view(), name='classroom-detail'),
 
+    # Subjects
     path('subjects/', subject_views.SubjectListCreateView.as_view(),
          name='subject-create'),
+    path('subjects/bulk/', subject_views.SubjectBulkUploadView.as_view(),
+         name='subject-bulk-upload'),
     path('subjects/<int:pk>/', subject_views.SubjectDetailView.as_view(),
          name='subject-detail'),
 
+    # Classroom-Subjects
     path('classroom-subjects/',
          classroom_subject_views.ClassroomSubjectListCreateView.as_view(), name='cs-create'),
     path('classroom-subjects/<int:pk>/',
          classroom_subject_views.ClasroomSubjectDetailView.as_view(), name='cs-detail'),
 
-    path('teacher/', teacher_views.TeacherListCreateView.as_view(),
+    # Teachers
+    path('teachers/', teacher_views.TeacherListCreateView.as_view(),
          name='teacher-list-create'),
-    path('teacher/<int:pk>/',
+    path('teachers/<int:pk>/',
          teacher_views.TeacherRetrieveUpdateDestroyView.as_view(), name='teacher-detail'),
 
+    # Students
     path('students/', StudentListCreateAPIView.as_view(),
          name='student-list-create'),
     path('students/<int:pk>/',
