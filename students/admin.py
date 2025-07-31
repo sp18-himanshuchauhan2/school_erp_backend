7fb88da8fa960a84d2bf945253453a2bca63455c
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-07-31 13:09:53 +0530

    updated students model on admin

--- students/admin.py
+++ students/admin.py
@@ -9,9 +9,15 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('user', 'roll_no', 'classroom', 'gender', 'dob', 'enrollment_date')
     search_fields = ('user__name', 'roll_no')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user__school=request.user.school)
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'user':
-            kwargs["queryset"] = User.objects.filter(role='STUDENT')
+            kwargs["queryset"] = User.objects.filter(role='STUDENT', school=request.user.school)
         
         elif db_field.name == 'classroom':
             if request.user.is_superuser:
